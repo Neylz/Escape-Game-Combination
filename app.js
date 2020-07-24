4aa9ed1e9b6aeac017b03e67a2323fd825ef093d
Neylz <56648332+Neylz@users.noreply.github.com>
2020-07-24 13:26:23 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+//Lettres : Space, W, A, S, D, F, G
+//keyCode :  32   87 65 83 68 70 71
+// https://keycode.info/
+
+const suite = [87, 65, 83]
+var suiteTest = []
+
+var anim = document.querySelectorAll('.circle');
+
+anim[0].style.animationPlayState = 'paused';
+
+function touche(e){
+    anim[0].style.animationPlayState = 'running';
+
+    function paused() {
+        anim[0].style.animationPlayState = 'paused';
+    }
+        
+    var touche = event.keyCode;
+
+    console.log(`keyCode "${touche}" pressed.`);
+    suiteTest.push(touche);
+
+    if (suite[suiteTest.length-1] !== suiteTest[suiteTest.length-1]) suiteTest = [];
+    console.log(`suiteTest = [${suiteTest}], suite = [${suite}]`);
+
+    if (suite[suite.length-1] == suiteTest[suite.length-1]) {
+        suiteTest = []
+        alert('good')
+    };
+
+    //setTimeout(paused(), 1000);
+}
+
