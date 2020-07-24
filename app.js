0b745c0d2ce74764a79153c74b3c3b4d2970e93e
Neylz_ <56648332+Neylz@users.noreply.github.com>
2020-07-24 20:18:43 +0200

    Add Images Files, Update Files
    Better animations, Images animations and correct some bugs

--- app.js
+++ app.js
@@ -5,18 +5,29 @@
 const suite = [87, 65, 83]
 var suiteTest = []
 
+correspondance = ['w','a','s','d','f','g','rightArrow','leftArrow','upArrow','downArrow','space']
+correspondance2= [87, 65, 83, 68, 70, 71, 39, 37, 38, 40, 32]
+correspondance3= ['gold.png','lead.png','phosphorerus.png','platinium.png','potassium.png','tin.png','zinc.png','','','','']
+
+
 var anim = document.querySelectorAll('.circle');
 
-anim[0].style.animationPlayState = 'paused';
+img = document.getElementById('image');
+
+paused;
 var paused = function () {
     anim[0].style.animationPlayState = 'paused';
+    img.src = ''
 }
 
+img.src = ''
 
 function touche(e){
     anim[0].style.animationPlayState = 'running';
         
     var touche = event.keyCode;
+
+    img.src = 'symbols/'+correspondance3[correspondance2.indexOf(touche)];
 
     console.log(`keyCode "${touche}" pressed.`);
     suiteTest.push(touche);
